be7e3413c4a758cfbfc4acf354e4bcc595762227
Alexis Finn <alexis.finn@bloomin.digital>
2021-06-07 18:05:59 +0200

    was trigerring shortcut accidentally, so prefix with leader

--- config/nvim/lua/plugin-configs/lazygit.lua
+++ config/nvim/lua/plugin-configs/lazygit.lua
@@ -1,4 +1,4 @@
-vim.api.nvim_set_keymap("n", "lg", ":LazyGit<CR>", {noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>lg", ":LazyGit<CR>", {noremap = true})
 
 vim.g.lazygit_fleating_window_use_plenary = 1
 vim.g.lazygit_floating_window_corner_chars = {'╭', '╮', '╰', '╯'} -- customize lazygit popup window corner characters
